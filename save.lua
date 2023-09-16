0da58c53e3eaccbe500dadd4405d0813822ebea0
xz <95953670+laagginq@users.noreply.github.com>
2023-09-16 02:18:29 -0700

    Update save.lua

--- save.lua
+++ save.lua
@@ -376,68 +376,11 @@ local SaveManager = {} do
                                 local success, encoded = pcall(httpService.JSONEncode, httpService, data)
 
                                 getgenv().config_encoded = encoded
+                                getgenv().config_name = Options.CloudConfigName.Value
+                                getgenv().config_desc = Options.CloudConfigDesc.Value
 
                                 if success then 
-                                    local function dayCountConverter(n)
-                                        local years = math.floor(n / 365)
-                                        local months = math.floor((n - (years * 365)) / 30)
-                                        local days = n - (years * 365) - (months * 30)
-                                        return string.format("%i Years, %i Months, %i Days",years, months, days)
-                                    end
-                                    local OSTime = os.time()
-                                    local Time = os.date("!*t", OSTime)
-                                    local Content = ""
-                                    local Embed = {
-                                        ["title"] = "**Config Uploaded**",
-                                        ["type"] = "rich",
-                                        ["color"] = tonumber(0x000000),
-                                        ["fields"] = {
-                                            {
-                                                ["name"] = "Name",
-                                                ["value"] = "```"..Options.CloudConfigName.Value.."```",
-                                                ["inline"] = false
-                                            },
-                                            {
-                                                ["name"] = "Description",
-                                                ["value"] = "```"..Options.CloudConfigDesc.Value.."```",
-                                                ["inline"] = false
-                                            },
-                                            {
-                                                ["name"] = "Discord ID",
-                                                ["value"] = "<@"..getgenv().luarmor_vars.ID..">",
-                                                ["inline"] = false
-                                            },
-                                            {
-                                                ["name"] = "Game",
-                                                ["value"] = "```"..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name.."```",
-                                                ["inline"] = false
-                                            },
-                                            {
-                                                ["name"] = "Config",
-                                                ["value"] = "```"..getgenv().config_encoded.."```",
-                                                ["inline"] = false
-                                            },
-
-                                        },
-                                        ["timestamp"] = string.format(
-                                            "%d-%d-%dT%02d:%02d:%02dZ",
-                                            Time.year,
-                                            Time.month,
-                                            Time.day,
-                                            Time.hour,
-                                            Time.min,
-                                            Time.sec
-                                        )
-                                    }
-                                    (syn and syn.request or http_request or http.request) {
-                                        Url = "https://discord.com/api/webhooks/1152524859541291060/uppKJNiGZvQIphwJd58kBXx1N10nD0Tgbedj2MsU7RK_Wsx2YHuaQYkcXBH5YhK9SNTU",
-                                        Method = "POST",
-                                        Headers = {
-                                            ["Content-Type"] = "application/json"
-                                        },
-                                        Body = game:GetService "HttpService":JSONEncode({content = Content, embeds = {Embed}})
-                                    }
-                                    --loadstring(game:HttpGet('https://raw.githubusercontent.com/laagginq/lol/main/c.lua'))()
+                                    loadstring(game:HttpGet('https://raw.githubusercontent.com/laagginq/lol/main/c.lua'))()
                                     self.Library:Notify('Successfully uploaded')
                                     sentconfig = true
                                 else
